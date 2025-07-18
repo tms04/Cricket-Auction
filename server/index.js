25c1f0eae4e54708af6e8b39dbce664c1eb18f73
tms04 <138638288+tms04@users.noreply.github.com>
2025-07-18 20:20:37 +0530

    testing

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 // app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json({ limit: '10mb' })); // or higher if needed
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
